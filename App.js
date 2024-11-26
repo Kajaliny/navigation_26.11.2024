78ac1ac8cd5b362c49fc8735146cbf906e6ba57d
Kajaliny <139700105+Kajaliny@users.noreply.github.com>
2024-11-26 14:08:46 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,43 +1,20 @@
 
 import './App.css';
-import { useState } from 'react';
-//import Calculator from './components/Calculator';
-import Students from './components/Students';
+import{BrowserRouter as Router, Routes, Route, Link, NavLink} from 'react-router-dom';
 
-
-
-function App(){
- const [theme,setTheme]=useState(true);
-  const [fsize,setFsize]=useState(10);
-  const darkTheme={
-    backgroundColor:"#282c34",
-    color:"white",
-    fontSize:fsize+"px"
-  
-  }
-  const lightTheme ={
-    backgroundColor: "white",
-    color:"#282c34",
-    fontSize:fsize+"px"
-  }
 
   
-  
+  function App(){
   return (
 
     <div className="App">
-      <header className="App-header" style={theme?lightTheme:darkTheme}>
-   
-      <Students></Students>
-         
-      <button onClick={()=>{setTheme(!theme)}}>{theme?"Go Dark":"Go Light"}</button>
-        <br/>
-        Font Size"<input type='range'
-        name="fontchg" min='10' max='50'
-        step='1' value={fsize} onChange={(e)=>{setFsize(e.target.value); console.log(fsize)}}/>
-
-     </header>
-     
+     <Router>
+      <Routes>
+        <Route path='/home' element={<Home/>}/>
+        <Route path='/about' element={<AboutUs/>}/>
+        <Route path='/contact' element={<Contactus/>}/>
+      </Routes>
+     </Router>
 
      
     </div>
