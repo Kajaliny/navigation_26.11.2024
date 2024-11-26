36021caa0c2920e6e8dd9b2ade0e0d39e4f42148
Kajaliny <139700105+Kajaliny@users.noreply.github.com>
2024-11-26 14:17:35 +0530

    Update2 App.js

--- App.js
+++ App.js
@@ -1,6 +1,10 @@
 
 import './App.css';
 import{BrowserRouter as Router, Routes, Route, Link, NavLink} from 'react-router-dom';
+import Contactus from './components/Contactus';
+import AboutUs from './components/AboutUs';
+import Home from './components/Home';
+
 
 
   
@@ -9,6 +13,14 @@ import{BrowserRouter as Router, Routes, Route, Link, NavLink} from 'react-router
 
     <div className="App">
      <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/home"> Home </Link></li>
+           <li> <Link to="/about"> About </Link></li>
+           <li> <Link to="/contact"> Contact </Link></li>
+        </ul>
+      </nav>
       <Routes>
         <Route path='/home' element={<Home/>}/>
         <Route path='/about' element={<AboutUs/>}/>
